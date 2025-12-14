02e03486f2e14203c52b841430aec562c4691385
Noornaj12 <noornajbano@gmail.com>
2025-12-14 21:35:21 +0530

    enhancements

--- src/app/components/forecast-card/forecast-card.ts
+++ src/app/components/forecast-card/forecast-card.ts
@@ -1,13 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoundPipe } from '../../pipes/round.pipe';
 
 @Component({
   selector: 'app-forecast-card',
   standalone: true,
-  imports: [CommonModule,RoundPipe],
-  templateUrl:'./forecast-card.html'
-  
+  imports: [CommonModule, RoundPipe],
+  templateUrl: './forecast-card.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ForecastCard {
   @Input() day!: any;
