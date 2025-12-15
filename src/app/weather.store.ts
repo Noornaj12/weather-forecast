28992ee35edff5be82ba48eca969b91af366048d
Noornaj12 <noornajbano@gmail.com>
2025-12-15 11:51:14 +0530

    enhancements

--- src/app/weather.store.ts
+++ src/app/weather.store.ts
@@ -1,46 +1,39 @@
 import { Injectable, signal, computed, effect } from '@angular/core';
 import { WeatherService } from './weather.service';
 import { City, WeatherForecast } from './weather.model';
+import { takeUntilDestroyed, toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { catchError, filter, of, switchMap, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class WeatherStore {
   // State signals
   city = signal<City | null>(null);
-  forecast = signal<WeatherForecast[]>([]);
   loading = signal<boolean>(false);
   error = signal<string | null>(null);
   cities: City[] = ['Birmingham', 'London', 'Cardiff'];
 
-  constructor(private api: WeatherService) {
-    // Effect: when city changes → load weather
-    effect(() => {
-      const selectedCity = this.city();
-      if (!selectedCity) {
-        this.forecast.set([]);
-        return;
-      }
-
-      this.loadWeather(selectedCity);
-    });
-  }
+  forecast = toSignal(
+    toObservable(this.city).pipe(
+      filter((city): city is City => !!city),
+      tap(() => {
+        this.loading.set(true);
+        this.error.set(null);
+      }),
+      switchMap((city) => {
+        return this.api.getForecast(city).pipe(
+          catchError((err) => {
+            this.error.set(err?.message || 'Failed to load weather');
+            return of([]);
+          })
+        );
+      }),
+      tap(() => this.loading.set(false)),
+      takeUntilDestroyed()
+    )
+  );
+  constructor(private api: WeatherService) {}
 
   setCity(city: City | null) {
     this.city.set(city);
-  }
-
-  private loadWeather(city: City) {
-    this.loading.set(true);
-    this.error.set(null);
-
-    this.api.getForecast(city).subscribe({
-      next: (data: WeatherForecast[]) => {
-        this.forecast.set(data);
-        this.loading.set(false);
-      },
-      error: (err: Error) => {
-        this.error.set(err?.message || 'Failed to load weather');
-        this.loading.set(false);
-      },
-    });
   }
 }
